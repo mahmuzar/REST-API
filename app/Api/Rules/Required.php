ba84eaba417d5797e39209055beb755286fd224a
Мурад Кадиев <m.kadiev@bryce.ru>
2018-07-25 01:30:21 +0300

    Правило для 'required' при валидации формы.

---
+++ app/Api/Rules/Required.php
@@ -0,0 +1,36 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Api\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Required implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return !empty($value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return trans('validation.required');
+    }
+}
