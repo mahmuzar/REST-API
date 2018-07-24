cff0eda2c9e913b3b33ef95872807115fda34598
Мурад Кадиев <m.kadiev@bryce.ru>
2018-07-25 01:29:47 +0300

    Базовый контроллер

---
+++ app/Api/Http/Controllers/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Api\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+   public function __construct() {
+       
+   }
+}
