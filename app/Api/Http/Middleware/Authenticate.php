de02a27eb84554493288736b5651d530ea7bd002
Мурад Кадиев <m.kadiev@bryce.ru>
2018-07-25 01:25:23 +0300

    Middleware - проверка прав для работы с API

---
+++ app/Api/Http/Middleware/Authenticate.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Проверяем авутентифицироват ли пользователь.
+ * Если нет, выбрасывается исключение AuthenticationException
+ * Иначе продолжается запрос
+ * 
+ * Этот middleware срабатывает при каждом обращении к API. Это обеспечивает 
+ * не санкцианированный доступ к API
+ * 
+ */
+namespace App\Api\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Http\Response;
+use Illuminate\Auth\AuthenticationException;
+
+class Authenticate {
+
+    /**
+     * The authentication guard factory instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @return void
+     */
+    public function __construct(Auth $auth) {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null) {
+        
+        if ($this->auth->guard($guard)->guest()) {
+            throw new AuthenticationException;
+        }
+        return $next($request);
+    }
+
+}
